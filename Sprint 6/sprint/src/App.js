9bb712cf06f31cc34fdb9261b894a2ad48b61e08
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-03-09 12:40:58 +0100

    Sprint 6 Exercise 1.2
    Remade the exercise

---
+++ Sprint 6/sprint/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Escena from './components/escena/escena.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <Escena/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Sprint 6/sprint/src/components/escena/escena.js
@@ -0,0 +1,3 @@
+export default function Escena() {
+    return <p>'El nostre heroi estava surant per lespai sideral quan a la llunyania va albirar una nau espacial'</p>;
+}
