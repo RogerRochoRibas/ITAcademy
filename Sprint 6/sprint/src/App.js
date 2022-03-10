62220c6aa228ecc48ca6543b14abd8f9010e5525
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-03-10 09:39:15 +0100

    Sprint 6 Exercise 2

--- Sprint 6/sprint/src/App.js
+++ Sprint 6/sprint/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import Escena from './components/escena/escena.js';
+import {Escena} from './components/escena/escena.js';
+import {data} from './components/escena/data.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-          <Escena/>
+          <Escena
+          scene = {data[0]}/>
+          
+          <Escena
+          scene = {data[1]}/>
+          
+          <Escena
+          scene = {data[2]}/>
+          
+          <Escena
+          scene = {data[3]}/>
     </div>
   );
 }
--- Sprint 6/sprint/src/components/escena/escena.js
+++ Sprint 6/sprint/src/components/escena/escena.js
@@ -1,3 +1,10 @@
-export default function Escena() {
+/* Exercise 1
+var Escena = function Escena() {
     return <p>'El nostre heroi estava surant per lespai sideral quan a la llunyania va albirar una nau espacial'</p>;
+}*/
+
+//Exercise 2
+var Escena = function Escena(props) {
+  return <p>{props.scene}</p>;
 }
+export {Escena}
