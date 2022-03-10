2045f93f451fd9839e103295af53c4dd1032e5fe
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-03-10 10:31:10 +0100

    Sprint 6 Exercise 3

--- Sprint 6/sprint/src/App.js
+++ Sprint 6/sprint/src/App.js
@@ -1,21 +1,31 @@
-import {Escena} from './components/escena/escena.js';
+import React from "react";
+import styled from "styled-components";
+import {Escena, Section} from './components/escena/escena.js';
 import {data} from './components/escena/data.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Section>
           <Escena
           scene = {data[0]}/>
+      </Section>
           
+      <Section>
           <Escena
           scene = {data[1]}/>
+      </Section>
           
+      <Section>
           <Escena
           scene = {data[2]}/>
+      </Section>
           
+      <Section>
           <Escena
           scene = {data[3]}/>
+      </Section>
     </div>
   );
 }
--- Sprint 6/sprint/src/components/escena/escena.js
+++ Sprint 6/sprint/src/components/escena/escena.js
@@ -1,10 +1,24 @@
+import React from "react";
+import styled from "styled-components";
 /* Exercise 1
 var Escena = function Escena() {
     return <p>'El nostre heroi estava surant per lespai sideral quan a la llunyania va albirar una nau espacial'</p>;
 }*/
 
-//Exercise 2
+/* Exercise 2
 var Escena = function Escena(props) {
   return <p>{props.scene}</p>;
-}
-export {Escena}
+}*/
+
+// Exercise 3
+const Section = styled.div`
+  border-color:black;
+  border-width:2px;
+  margin:1em;
+  border-radius:30px;
+  border-style:solid`;
+
+var Escena = function Escena(props) {
+    return <p>{props.scene}</p>;}
+
+export {Escena,Section}
