0d4f7082b514bd14319c39383f6501a697436b21
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-03-14 12:59:19 +0100

    Sprint 6 Exercise 3.2

--- Sprint 6/sprint/src/App.js
+++ Sprint 6/sprint/src/App.js
@@ -1,32 +1,12 @@
-import React from "react";
-import styled from "styled-components";
-import {Escena, Section} from './components/escena/escena.js';
-import {data} from './components/escena/data.js';
+import {Escena, Buttons} from './components/escena/escena.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Section>
-          <Escena
-          scene = {data[0]}/>
-      </Section>
-          
-      <Section>
-          <Escena
-          scene = {data[1]}/>
-      </Section>
-          
-      <Section>
-          <Escena
-          scene = {data[2]}/>
-      </Section>
-          
-      <Section>
-          <Escena
-          scene = {data[3]}/>
-      </Section>
-    </div>
+      <Buttons/>
+      <Escena/>
+      </div>
   );
 }
 
--- Sprint 6/sprint/src/components/escena/escena.js
+++ Sprint 6/sprint/src/components/escena/escena.js
@@ -1,5 +1,5 @@
-import React from "react";
 import styled from "styled-components";
+import {data} from './data.js';
 /* Exercise 1
 var Escena = function Escena() {
     return <p>'El nostre heroi estava surant per lespai sideral quan a la llunyania va albirar una nau espacial'</p>;
@@ -16,9 +16,45 @@ const Section = styled.div`
   border-width:2px;
   margin:1em;
   border-radius:30px;
-  border-style:solid`;
+  border-style:solid;`;
+  
+export let ListItem = styled.p`
+  max-width: 90%;
+  padding: 0.5rem;
+  font-size: ${props => props.active ? '1.25rem': '1rem'};
+  border: ${props => props.active ? '0.15rem solid #000000': 'none'};
+  border-radius: 2rem;
+  background: ${props => props.active ? '#FF8C69' : 'hsla(0, 0%, 100%, 0.6)'};
+  list-style: none;`
+;
 
-var Escena = function Escena(props) {
-    return <p>{props.scene}</p>;}
+function Escena(props) {
+    return data.map((data) => <Section><ListItem>{data}</ListItem></Section>);}
 
-export {Escena,Section}
+// Exercise 4
+
+var activeLine = 1
+
+function ClickedPrevious() {
+  console.log("Anterior");
+  if (activeLine > 1) {
+    activeLine--
+  }
+console.log('activeLine:', activeLine);
+}
+
+function ClickedNext() {
+  console.log("Siguiente")
+  if (activeLine < 4) {
+    activeLine++
+  }
+  console.log('activeLine:', activeLine);
+}
+function Buttons() {
+  return <div>
+      <button onClick={ClickedPrevious}>Previous</button>
+      <button onClick={ClickedNext}>Next</button>
+    </div>
+}
+
+export {Escena, Section, Buttons, ClickedPrevious, ClickedNext}
