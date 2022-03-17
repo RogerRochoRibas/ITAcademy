df4088767f52c4b1a3b166c14930eb6ed41b35b9
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-03-17 13:19:57 +0100

    Sprint 6 Exercise 3.4
    Solo falta mejorar el formato

--- Sprint 6/sprint/src/App.js
+++ Sprint 6/sprint/src/App.js
@@ -6,70 +6,46 @@ import './App.css';
 
 function App() {
 
-  const Section = styled.div/*.attrs({active}={id}==={counter})*/
+  const Section = styled.div
   `border-color:black;
   border-width:2px;
   margin:1em;
-  background: ${props => props.active ? 'hsla(0, 50%, 90%, 0.6)' : 'white'};
+  background: ${props => props.active ? '#FF8C69' : 'hsla(0, 90%, 100%)'};
   border-radius:30px;
   border-style:solid;
   padding: 0.5rem;
   max-width: 90%;`;
 
-  var [counter, setCounter] = React.useState(1)
-  function ClickedPrevious() {
-    console.log("Before Back:", counter);
-      if (counter>1){
-        let oldLine = document.getElementById(counter)
-        oldLine.classList.remove('active')
-        setCounter(counter--);
-        let newLine = document.getElementById(counter)
-        newLine.classList.add('active')
-      } else console.log('too low')
-    console.log('After Back:', counter);
+  var [counter, setCounter] = React.useState(0)
+
+  const ClickedMinus =()=> {
+        if (counter>0){
+          setCounter(counter-1);
+          console.log('counter: ',counter)
+        }
   }
   
-  function ClickedNext() {
-    console.log("Clicked Next: ", counter);
-      if (counter<4) {
-        let oldLine = document.getElementById(counter)
-        oldLine.classList.remove('active')
-        setCounter(counter++);
-        let newLine = document.getElementById(counter)
-        newLine.classList.add('active')
-      } else console.log('too high')
-    console.log('counter(next):', counter);
+function ClickedPlus() {
+    if (counter<4){
+      setCounter(counter+1);
+    }
   }
+  
   function Buttons() {
     return <div>
-        <button onClick={ClickedPrevious}>Previous</button>
-        <button onClick={ClickedNext}>Next</button>
-      </div>
+        <button onClick={() => ClickedMinus()}>Previous</button>
+        <button onClick={() => ClickedPlus()}>Next</button>
+    </div>
   }
-  function Escena(props) {
-      return data.map((data,i) => <Section id={i+1}>{i}: {data}</Section>);}
-  /*function Escena() {
-    return data.map(element,index)(
-      <ListItem> 
-          {data} 
-      </ListItem>
-  );}
-
-  let ListItem = styled.li`
-    max-width: 90%;
-    padding: 0.5rem;
-    font-size: ${props => props.active ? '1.25rem': '1rem'};
-    border: ${props => props.active ? '0.15rem solid #000000': 'none'};
-    border-radius: 2rem;
-    background: ${props => props.active ? '#FF8C69' : 'hsla(0, 0%, 100%, 0.6)'};
-    list-style: none`;*/
+  function Escena() {
+      return data.map((data,i) => <Section active={i===counter} key={i} id={i+1}>{i+1}: {data}</Section>);}
 ;
   
   return (
     <div className="App">
       <Buttons/>
       <Escena/>
-      </div>
+    </div>
   );
 }
 
