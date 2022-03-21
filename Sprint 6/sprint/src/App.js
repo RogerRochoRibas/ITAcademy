76354dbfce64c8eff3a707c0c4b306b810a03eda
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-03-21 10:59:32 +0100

    Sprint 6 Exercise 6

--- Sprint 6/sprint/src/App.js
+++ Sprint 6/sprint/src/App.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import React from "react"
 import {data} from './components/escena/data.js';
 import './App.css';
-import img1 from './img/1.jpg'
 
 function App() {
 
@@ -12,7 +11,7 @@ function App() {
   function Buttons() {
     const ClickedPrevious=()=> {
       if (counter >0) {
-        setCounter(counter-1);  
+        setCounter(counter-1);
       }
     }
   
@@ -29,38 +28,39 @@ function App() {
   }
   // Texts
   const Section = styled.div
-  `border-color:black;
-  border-width:2px;
-  margin:1em;
-  background: ${props => props.active ? '#FF8C69' : 'white'};
-  border-radius:30px;
-  border-style:solid;
-  padding: 0.5rem;
-  max-width: 90%;`;
+    `border-color:black;
+    border-width:2px;
+    margin:1em;
+    background: ${props => props.active ? '#FF8C69' : 'white'};
+    border-radius:30px;
+    border-style:solid;
+    padding: 0.5rem;
+    max-width: 90%;`;
 
   function Scene() {
-    return data.map((data,i) => <Section active={i===counter} key={i} id={i+1}>{i+1}: {data.txt}</Section>);
+    return data.map((data,i) => <Section active={i===counter} key={i} id={i+1}>{data.txt}</Section>);
   }
+  
   // Background
+
   var Body = styled.div`
-  height:100%;
-  background-size:cover;
-  background-image:url(${img1};
+    height:100%;
+    background-size:cover;
+    background-image:url(${data[counter].img});
   `;
 
   // Welcome
   var [welcome, setWelcome] = React.useState(true)
-  const Continue=()=> {setWelcome(false)}
+  const Continue=() => {setWelcome(false)}
   if (welcome===true) {
     return <div className="App">
       <button onClick={() => Continue()}>Continue</button>
-      <p>In this page we use React components to show 4 lines of text. You can use the Previous and Next buttons to choose which line is highlighted.</p>
+      <p>In this page we use React components to show lines of text from an array. You can use the Previous and Next buttons to choose which line is highlighted.</p>
     </div>
-  } 
-  // Main Page
+  } // Main Page
   else {
     return (
-      <Body className="App" id="something">
+      <Body className="App" id="Body">
         <Buttons/>
         <Scene/>
       </Body>
--- Sprint 6/sprint/src/components/escena/data.js
+++ Sprint 6/sprint/src/components/escena/data.js
@@ -1,10 +1,10 @@
 export const data = [
     {txt:"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
-    img:"../img/1.jpg"},
+    img:"/static/media/1.7e7a855bd1d2da50d556.jpg"},
     {txt:"Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
-    img:"../img/2.jpg"},
+    img:"/static/media/2.a53acf76d831e32a3a4e.jpg"},
     {txt:"L'heroi va decidir travessar la porta que el portava a casa",
-    img:"../img/3.jpg"},
+    img:"/static/media/3.bb53871cc174f4ca528e.jpg"},
     {txt:"Mentrestant, altres herois no van tenir tanta sort en la seva elecció ...",
-    img:"../img/4.jpg"},
+    img:"/static/media/4.2995f9ca0920650627ca.jpg"},
   ]
